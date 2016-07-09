c4115c9f26291da213f79b495111dd080511f5b7
itsFrank <frankobrien11@yahoo.ca>
2016-07-09 20:09:01 +0200

    fixing repo

---
+++ test/proptest.ts
@@ -0,0 +1,5 @@
+import * as Vue from 'vue'
+import { expect } from 'chai'
+
+import { Prop } from '../src/prop'
+import { VueComponent } from '../src/vuecomponent'
--- typings/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="globals/chai/index.d.ts" />
-/// <reference path="globals/mocha/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/vue/index.d.ts" />
